15eaa8d69649babdf1b4882644abf9fb6bf5e31f
Thiago Ururay <thiago.araujo@dtmtec.com.br>
2019-12-18 01:16:17 -0300

    adds category, product and productimage models

--- store/settings.py
+++ store/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djmoney',
     'toshop'
 ]
 
@@ -145,5 +146,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = '/toshop/uploads/'
 
 django_heroku.settings(locals())
--- toshop/models.py
+++ toshop/models.py
@@ -1,6 +1,28 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
+
+def product_directory_path(instance, filename):
+    return 'product_{0}/{1}-%Y-%m-%d'.format(instance.product.id, filename)
 
 class StaticPages(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False, verbose_name="Título")
     content = models.TextField(null=False, blank=False, verbose_name="Conteúdo")
     page_id = models.CharField(max_length=15, unique=True, null=False, blank=False, verbose_name="Identificador")
+
+class Category(models.Model):
+    name = models.CharField(max_length=15, null=False, unique=True, verbose_name='Categoria')
+    parent = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, verbose_name="Categoria Pai")
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Nome do Produto")
+    price = MoneyField(max_digits=7, decimal_places=2, default_currency='BRL', null=False, blank=False)
+    description = models.TextField(verbose_name='Descrição', null=False, blank=False)
+    category = models.ForeignKey('Category', on_delete=models.DO_NOTHING, null=False, verbose_name="Categoria")
+    active = models.BooleanField(verbose_name="Ativo", default=False)
+    discount = models.BooleanField(verbose_name="Desconto", default=False)
+    discount_value = MoneyField(max_digits=7, decimal_places=2, default_currency='BRL', null=True)
+
+class ProductImage(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, null=False, verbose_name="Produto")
+    image = models.FileField(upload_to=product_directory_path, verbose_name='Imagem')
+    cover = models.BooleanField(default=False, verbose_name="Capa")
